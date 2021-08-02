0ca407d06f17a3ca409ce42ffceab273dbf1bf1d
AndroBuddy <anirudhchimpidi8@gmail.com>
2021-08-02 11:51:30 +0530

    Setup app theme and colors
    Signed-off-by: AndroBuddy <anirudhchimpidi8@gmail.com>

--- app/src/main/java/com/orca/weather/ui/theme/Theme.kt
+++ app/src/main/java/com/orca/weather/ui/theme/Theme.kt
@@ -5,30 +5,28 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Black700,
+    primaryVariant = Black700,
+    secondary = Color.White,
+    surface = Black700,
+    onSurface = Color.White,
+    onPrimary = Color.White
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
+    primary = Color.White,
+    primaryVariant = Color.White,
+    secondary = Black700,
     surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onSurface = Black400,
+    onPrimary = Black400
 )
 
 @Composable
-fun WeatherTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun WeatherTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
