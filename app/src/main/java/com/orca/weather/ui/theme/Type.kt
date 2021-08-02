07b0c8b479158fbda2d2f30cebc9a565994ba64b
AndroBuddy <anirudhchimpidi8@gmail.com>
2021-08-02 11:43:42 +0530

    Setup drawables and fonts
    Signed-off-by: AndroBuddy <anirudhchimpidi8@gmail.com>

--- app/src/main/java/com/orca/weather/ui/theme/Type.kt
+++ app/src/main/java/com/orca/weather/ui/theme/Type.kt
@@ -2,27 +2,40 @@ package com.orca.weather.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.orca.weather.R
 
 // Set of Material typography styles to start with
+val UrbanistLight = FontFamily(Font(R.font.urbanist_light))
+val UrbanistRegular = FontFamily(Font(R.font.urbanist_regular))
+val UrbanistBold = FontFamily(Font(R.font.urbanist_bold))
+
 val Typography = Typography(
+    h1 = TextStyle(
+        fontFamily = UrbanistBold,
+        fontSize = 88.sp,
+        letterSpacing = 0.5.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = UrbanistBold,
+        fontSize = 16.sp,
+        letterSpacing = 0.sp
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = UrbanistLight,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = UrbanistBold,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontFamily = UrbanistRegular,
+        fontSize = 10.sp,
+        letterSpacing = 0.sp
     )
-    */
 )
